518bec2904ef859afe06094f6a7c62d3463378f9
Ajey Gore <ajeygore@gmail.com>
2022-12-04 09:38:28 +0800

    Tweaked the sentences

---
+++ assets/scripts/wardley-strategy-simple.js
@@ -0,0 +1,34 @@
+function wardleyStrategy() {
+    // create an array with following words
+    // digital first, agile, open, innovative, efficiency, competitive advantage, ecosystem, networked, collaborative,  learning organisation, social media, revolution, cloud based, big data, secure, internet of things, growth, value, customer focused, digital business, disruptive, data leaders, big data, insight from data, platform, sustainable, revolution, culture.
+    // and create a string template with following sentence
+    // Our strategy is [..]. We will lead a [..] effort of the market through our use of [..] and [..]  to build a [..]. By being both [..] and [..], our [..] approach will drive [..] throughout the organisation. Synergies between our [..] and [..] will enable us to capture the upside by becoming [..] in a [..] world. These transformations combined with [..] due to our [..] will create a [..] through [..] and [..].
+    // and insert unique random words from array into string template and return the string
+
+    // create an array with following words
+    var words = ["digital first", "agile", "open", "innovative", "efficiency", "competitive advantage", "ecosystem", "networked", "collaborative", "learning organisation", "social media", "revolution", "cloud based", "big data", "secure", "internet of things", "growth", "value", "customer focused", "digital business", "disruptive", "data leaders", "big data", "insight from data", "platform", "sustainable", "revolution", "culture driven"];
+    // and create a string template with following sentence
+    //"Our strategy is [..]. We will lead a [..] effort of the market through our use of [..] and [..]  to build a [..]. By being both [..] and [..], our [..] approach will drive [..] throughout the organisation. Synergies between our [..] and [..] will enable us to capture the upside by becoming [..] in a [..] world. These transformations combined with [..] due to our [..] will create a [..] through [..] and [..].";
+    var template = "Our strategy is [..]. We will lead a [..] effort of the market through our use of [..] and [..]  to build a [..] business. By being both [..] and [..], our [..] approach will drive [..] throughout the organisation. Synergies between our [..] and [..] will enable us to capture the upside by becoming [..] in a [..] world. These transformations combined with [..] due to our [..] will create a [..] through [..] and [..].";
+    // and insert unique random words from array into string template and return the html with words in bold
+    var result = template.replace(/\[\.\.\]/g, function () {
+        return words.splice(Math.floor(Math.random() * words.length), 1);
+    });
+    return result;
+
+
+    // and insert unique random words from array into string template and return the string
+
+}
+
+
+function wardleyWords() {
+    // create an array with following words
+    // digital first, agile, open, innovative, efficiency, competitive advantage, ecosystem, networked, collaborative,  learning organisation, social media, revolution, cloud based, big data, secure, internet of things, growth, value, customer focused, digital business, disruptive, data leaders, big data, insight from data, platform, sustainable, revolution, culture.
+    // and return the array
+
+    // create an array with following words
+    var words = ["digital first", "agile", "open", "innovative", "efficiency", "competitive advantage", "ecosystem", "networked", "collaborative", "learning organisation", "social media", "revolution", "cloud based", "big data", "secure", "internet of things", "growth", "value", "customer focused", "digital business", "disruptive", "data leaders", "big data", "insight from data", "platform", "sustainable", "revolution", "culture"];
+    // and return the array
+    return words;
+}
--- assets/scripts/wardley-strategy.js
+++ assets/scripts/wardley-strategy.js
@@ -1,39 +1,5 @@
-function wardleyStrategy() {
-  // create an array with following words
-    // digital first, agile, open, innovative, efficiency, competitive advantage, ecosystem, networked, collaborative,  learning organisation, social media, revolution, cloud based, big data, secure, internet of things, growth, value, customer focused, digital business, disruptive, data leaders, big data, insight from data, platform, sustainable, revolution, culture.
-    // and create a string template with following sentence
-    // Our strategy is [..]. We will lead a [..] effort of the market through our use of [..] and [..]  to build a [..]. By being both [..] and [..], our [..] approach will drive [..] throughout the organisation. Synergies between our [..] and [..] will enable us to capture the upside by becoming [..] in a [..] world. These transformations combined with [..] due to our [..] will create a [..] through [..] and [..].
-    // and insert unique random words from array into string template and return the string
 
-    // create an array with following words
-    var words = ["digital first", "agile", "open", "innovative", "efficiency", "competitive advantage", "ecosystem", "networked", "collaborative", "learning organisation", "social media", "revolution", "cloud based", "big data", "secure", "internet of things", "growth", "value", "customer focused", "digital business", "disruptive", "data leaders", "big data", "insight from data", "platform", "sustainable", "revolution", "culture driven"];
-    // and create a string template with following sentence
-    //"Our strategy is [..]. We will lead a [..] effort of the market through our use of [..] and [..]  to build a [..]. By being both [..] and [..], our [..] approach will drive [..] throughout the organisation. Synergies between our [..] and [..] will enable us to capture the upside by becoming [..] in a [..] world. These transformations combined with [..] due to our [..] will create a [..] through [..] and [..].";
-    var template = "Our strategy is [..]. We will lead a [..] effort of the market through our use of [..] and [..]  to build a [..] business. By being both [..] and [..], our [..] approach will drive [..] throughout the organisation. Synergies between our [..] and [..] will enable us to capture the upside by becoming [..] in a [..] world. These transformations combined with [..] due to our [..] will create a [..] through [..] and [..].";
-    // and insert unique random words from array into string template and return the html with words in bold
-    var result = template.replace(/\[\.\.\]/g, function() { return words.splice(Math.floor(Math.random() * words.length), 1); });
-    return result;
-
-
-    // and insert unique random words from array into string template and return the string
-
-}
-
-
-function wardleyWords(){
-    // create an array with following words
-    // digital first, agile, open, innovative, efficiency, competitive advantage, ecosystem, networked, collaborative,  learning organisation, social media, revolution, cloud based, big data, secure, internet of things, growth, value, customer focused, digital business, disruptive, data leaders, big data, insight from data, platform, sustainable, revolution, culture.
-    // and return the array
-
-    // create an array with following words
-    var words = ["digital first", "agile", "open", "innovative", "efficiency", "competitive advantage", "ecosystem", "networked", "collaborative", "learning organisation", "social media", "revolution", "cloud based", "big data", "secure", "internet of things", "growth", "value", "customer focused", "digital business", "disruptive", "data leaders", "big data", "insight from data", "platform", "sustainable", "revolution", "culture"];
-    // and return the array
-    return words;
-}
-
-
-
-function wardleyNouns(){
+function wardleyNouns() {
     // create an array with following words
     // competitive advantage, learning organization, big data, growth, value, digital business, revolution, culture, ecosystem, internet of things, insight from data, virtual reality, artificial intelligence, blockchain, design thinking, digital transformation
     // and return the array
@@ -45,7 +11,7 @@ function wardleyNouns(){
     return words;
 }
 
-function wardleyPluralNouns(){
+function wardleyPluralNouns() {
     //create an with following words
     //social media leaders, data leaders, digital leaders
     // and return the array
@@ -57,7 +23,7 @@ function wardleyPluralNouns(){
 
 }
 
-function wardleyAdjectives(){
+function wardleyAdjectives() {
     //create an with following words
     //digital first, agile, open, innovative, efficient, collaborative, networked, customer focused, disruptive, sustainable, culture driven
     // and return the array
@@ -68,7 +34,15 @@ function wardleyAdjectives(){
     return words;
 }
 
-function wardleyStrategyGenerator(){
+function getNounsPluralNounsAndAdjectives(){
+    var nouns = wardleyNouns();
+    var pluralNouns = wardleyPluralNouns();
+    var adjectives = wardleyAdjectives();
+    var result = nouns.concat(pluralNouns, adjectives);
+    return result;
+}
+
+function wardleyStrategyGenerator() {
     // take following sentence and templatize with
     //Our strategy is <%=adjective%>. We will lead <%=indef_adjective%> effort of the market through our use of <%=noun%> and <%=plural_noun%> to build <%=indef_noun%>. By being both <%=adjective%> and <%=adjective%>, our <%=adjective%> approach will drive <%=noun%> throughout the organization. Synergies between our <%=noun%> and <%=noun%> will enable us to capture the upside by becoming <%=adjective%> in <%=indef_adjective%> world. These transformations combined with <%=noun%> due to our <%=plural_noun%> will create <%=indef_noun%> through <%=noun%> and <%=plural_noun%>.
     // and insert unique random words from array into string template and return the string
@@ -77,39 +51,44 @@ function wardleyStrategyGenerator(){
     var template = "Our strategy is <%=adjective%>. We will lead <%=indef_adjective%> effort of the market through our use of <%=noun%> and <%=plural_noun%> to build <%=indef_noun%>. By being both <%=adjective%> and <%=adjective%>, our <%=adjective%> approach will drive <%=noun%> throughout the organization. Synergies between our <%=noun%> and <%=noun%> will enable us to capture the upside by becoming <%=adjective%> in <%=indef_adjective%> world. These transformations combined with <%=noun%> due to our <%=plural_noun%> driven mission-oriented approach will create <%=indef_noun%> through <%=noun%> and <%=plural_noun%>.";
     // and insert unique random words from array into string template and return the string
 
+
+
+
     var nouns = wardleyNouns();
     var pluralNouns = wardleyPluralNouns();
     var adjectives = wardleyAdjectives();
-    var result = template.replace(/<%=adjective%>/g, function() { return adjectives.splice(Math.floor(Math.random() * adjectives.length), 1); });
- result = result.replace(/<%=indef_adjective%>/g, function() { return generateIndefiniteArticlePrefix(adjectives.splice(Math.floor(Math.random() * adjectives.length)), 1); });
-    result = result.replace(/<%=noun%>/g, function() { return nouns.splice(Math.floor(Math.random() * nouns.length), 1); });
-    result = result.replace(/<%=plural_noun%>/g, function() { return pluralNouns.splice(Math.floor(Math.random() * pluralNouns.length), 1); });
-    result = result.replace(/<%=indef_noun%>/g, function() { return nouns.splice(Math.floor(Math.random() * nouns.length), 1); });
-
+    var result = template.replace(/<%=adjective%>/g, function () {
+        return adjectives.splice(Math.floor(Math.random() * adjectives.length), 1);
+    });
+    result = result.replace(/<%=indef_adjective%>/g, function () {
+        return generateIndefiniteArticlePrefix(adjectives.splice(Math.floor(Math.random() * adjectives.length), 1));
+    });
+    result = result.replace(/<%=noun%>/g, function () {
+        return nouns.splice(Math.floor(Math.random() * nouns.length), 1);
+    });
+    result = result.replace(/<%=plural_noun%>/g, function () {
+        return pluralNouns.splice(Math.floor(Math.random() * pluralNouns.length), 1);
+    });
+    result = result.replace(/<%=indef_noun%>/g, function () {
+        return nouns.splice(Math.floor(Math.random() * nouns.length), 1);
+    });
 
 
     return result;
 
 }
 
-function generateIndefiniteArticlePrefix(inputWord){
+function generateIndefiniteArticlePrefix(inputWord) {
 
     let testString = inputWord.toString();
     // check if input word starts with vowel
-    if ( testString.toLowerCase().at(0) === 'a'
-    || testString.toLowerCase().at(0) === 'e'
-    || testString.toLowerCase().at(0) === 'i'
-    || testString.toLowerCase().at(0) === 'o'
-    || testString.toLowerCase().at(0) === 'u' ) {
+    if (testString.toLowerCase().at(0) === 'a' || testString.toLowerCase().at(0) === 'e' || testString.toLowerCase().at(0) === 'i' || testString.toLowerCase().at(0) === 'o' || testString.toLowerCase().at(0) === 'u') {
         // if it starts with vowel then return "an"
         return "an " + inputWord;
-    }
-    else {
+    } else {
         // if it starts with consonant then return "a"
         return "a " + inputWord;
     }
-
-
 
 
 }
